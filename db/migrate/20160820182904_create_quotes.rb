17902409985a386bc6e7712f83009ce4263d094e
arrC <naveen@arrc.in>
2016-08-21 15:46:42 +0530

    chaged quote cite column name to author & some minor changes in editor form

--- db/migrate/20160820182904_create_quotes.rb
+++ db/migrate/20160820182904_create_quotes.rb
@@ -2,7 +2,7 @@ class CreateQuotes < ActiveRecord::Migration[5.0]
   def change
     create_table :quotes do |t|
       t.text :body
-      t.string :cite
+      t.string :author
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -196,7 +196,7 @@ ActiveRecord::Schema.define(version: 20160820182904) do
 
   create_table "quotes", force: :cascade do |t|
     t.text     "body"
-    t.string   "cite"
+    t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
