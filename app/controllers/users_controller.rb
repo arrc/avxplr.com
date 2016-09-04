83db865095b5a2e78568d55ba0b348c8a5a4cecf
arrC <naveen@arrc.in>
2016-09-04 23:55:45 +0530

    profile: fixed the order issue in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @shots = @user.shots.limit(10)
+    @shots = @user.shots.order("created_at DESC").limit(10)
     @fav_shots = @user.favorites.where(favoritable_type: "Shot")
     @fav_aircrafts = @user.favorites.where(favoritable_type: "Aircraft")
   end
