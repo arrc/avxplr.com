55e0810f89aa3b672b7f401350fd7044006591c9
arrC <naveen@arrc.in>
2016-09-04 23:41:28 +0530

    profile: showing latest submit shots.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @shots = @user.shots.limit(10)
+    @fav_shots = @user.favorites.where(favoritable_type: "Shot")
+    @fav_aircrafts = @user.favorites.where(favoritable_type: "Aircraft")
   end
 end
