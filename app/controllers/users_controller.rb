6b992ff05a6f717ddae81e56bc1ca813716c8069
arrC <naveen@arrc.in>
2016-09-05 17:31:06 +0530

    profile: controller action for loading favorites working now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+
+    # load 10 recent shots posted by user
     @shots = @user.shots.order("created_at DESC").limit(10)
-    @fav_shots = @user.favorites.where(favoritable_type: "Shot")
-    @fav_aircrafts = @user.favorites.where(favoritable_type: "Aircraft")
+
+    # all shots favorited by user
+    fav_shot_ids = @user.favorites.where(favoritable_type: "Shot").order('created_at DESC').pluck(:favoritable_id)
+    @fav_shots = Shot.find(fav_shot_ids)
+
+    # all aircrafts favorited by user
+    fav_aircraft_ids = @user.favorites.where(favoritable_type: "Aircraft").order('created_at DESC').pluck(:favoritable_id)
+    @fav_aircrafts = Aircraft.find(fav_aircraft_ids)
   end
 end
